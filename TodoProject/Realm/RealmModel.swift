c0e2cba151bb1f7997660f1d463010e2329e5694
nhyeonjeong <hjnam5962@gmail.com>
2024-02-15 22:37:54 +0900

    [FEAT] realmModel로 todoTable생성 #5

--- TodoProject/Realm/RealmModel.swift
+++ TodoProject/Realm/RealmModel.swift
@@ -6,8 +6,24 @@
 //
 
 import Foundation
+import RealmSwift
 
 class TodoTable: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var memoTitle: String
+    @Persisted var memo: String?
+    @Persisted var deadline: String
+    @Persisted var tag: String?
+    @Persisted var priority: Int // 우선순위는 Int타입으로
     
+    init(memoTitle: String, memo: String? = nil, deadline: String, tag: String? = nil, priority: Int) {
+        
+        self.init()
+        self.memoTitle = memoTitle
+        self.memo = memo
+        self.deadline = deadline
+        self.tag = tag
+        self.priority = priority
+    }
 }
 
